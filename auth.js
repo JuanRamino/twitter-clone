bebbc71f59acaf46a9569216bbfef612d6545cdf
Juan Ramino <juan.ramino@gmail.com>
2015-08-05 11:05:33 +0200

    Add passport-local module. Implement local strategy to passport

--- auth.js
+++ auth.js
@@ -1,6 +1,9 @@
+'use strict';
+
 var _ = require('lodash')
   , passport = require('passport')
-  , fixtures = require('./fixtures');
+  , fixtures = require('./fixtures')
+  , LocalStrategy = require('passport-local').Strategy;
 
 
 passport.serializeUser(function(user, done) {
@@ -16,6 +19,23 @@ passport.deserializeUser(function(id, done) {
 
   done(null, user);
 });
+
+// http://passportjs.org/docs/username-password
+function verify(username, password, done) {
+  var user = _.find(fixtures.users, 'id', username);
+
+  if (!user) {
+    return done(null, false, { message: 'Incorrect username.' });
+  }
+
+  if (user.password !== password) {
+    return done(null, false, { message: 'Incorrect password.' });
+  }
+
+  done(null, user);
+}
+
+passport.use(new LocalStrategy(verify));
 
 
 module.exports = passport;
